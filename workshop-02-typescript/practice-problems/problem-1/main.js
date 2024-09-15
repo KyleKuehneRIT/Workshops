b4157ad2d095efe0737bee5eb63759b605b30721
Kyle <kdk3924@g.rit.edu>
2024-09-15 12:05:36 -0400

    Problem 1

---
+++ workshop-02-typescript/practice-problems/problem-1/main.js
@@ -0,0 +1,10 @@
+// Fill in for exercises!
+var userName = "Kyle";
+var favoriteFoods = ["Kiwis", "Pizza", "Calamari"];
+var user = {
+    userName: userName,
+    age: 20,
+    favoriteFoods: favoriteFoods
+};
+console.log(user);
+console.log("Food: " + favoriteFoods[0]);
--- workshop-02-typescript/practice-problems/problem-1/main.ts
+++ workshop-02-typescript/practice-problems/problem-1/main.ts
@@ -1,1 +1,14 @@
 // Fill in for exercises!
+
+let userName : string = "Kyle";
+
+let favoriteFoods: string[] = ["Kiwis", "Pizza", "Calamari"]
+
+const user: {userName: string, age: number, favoriteFoods: string[]} = {
+    userName: userName,
+    age: 20,
+    favoriteFoods: favoriteFoods
+};
+
+console.log(user)
+console.log("Food: " + favoriteFoods[0]);
